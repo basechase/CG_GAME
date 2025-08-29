1c7d73473162695eaa4012a1aa669c5ee72fb982
basechase <chase7y@gmail.com>
2025-08-29 12:53:16 -0500

    fixed UV thing
    have to make it a square again, add correct uv textures, make a cube

--- Source/CG_GAME/ProcdeduralCubeActor.cpp
+++ Source/CG_GAME/ProcdeduralCubeActor.cpp
@@ -22,26 +22,28 @@ void AProcdeduralCubeActor::BeginPlay()
 
 	Positions.Add(FVector(0, 50, -50));		// 1st vert's position
 	Positions.Add(FVector(0, -50, -50));	// 2nd vert's position
-	Positions.Add(FVector(0, -50, 50));	// 3rd vert's position
+	Positions.Add(FVector(0, -50, 50));		// 3rd vert's position
+	//Positions.Add(FVector(0, 50, 50));
 
-	Uvs.Add({1,0});
-	Uvs.Add({0,1});
 	Uvs.Add({0,0});
+	Uvs.Add({1,1});
+	Uvs.Add({1,0});
 
 	TrianglesIndices.Add(0);
 	TrianglesIndices.Add(1);
 	TrianglesIndices.Add(2);
-	
-	TrianglesIndices.Add(0);
-	
-	TrianglesIndices.Add(2);
-	TrianglesIndices.Add(1);
+	//TrianglesIndices.Add(3);
+
+	//TrianglesIndices.Add(0);
+	//TrianglesIndices.Add(3);
+	//TrianglesIndices.Add(2);
+	//TrianglesIndices.Add(1);
 
 	
 
 
 	// tells the engine to generate the mesh using the given data
-	ProceduralMesh->CreateMeshSection(0, Positions, TrianglesIndices, TArray<FVector>(), TArray<FVector2D>(), TArray<FColor>(), TArray<FProcMeshTangent>(), true);
+	ProceduralMesh->CreateMeshSection(0, Positions, TrianglesIndices, TArray<FVector>(), Uvs, TArray<FColor>(), TArray<FProcMeshTangent>(), true);
 	
 
 
